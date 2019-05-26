ce1743cec9d6f18abc54ed14350679c80fb42425
qizhi.wang <qizhi.wang@rograndec.com>
2019-05-26 16:19:58 +0800

    chapter01 commit

--- chapter01/addCount.py
+++ chapter01/addCount.py
@@ -1,11 +1,6 @@
 # 输入图片，在图片上加上数字然后输出
 from PIL import Image, ImageFont, ImageDraw
 
-if __name__ == '__main__':
-
-    print("success");
-
-
 def addcount():
     outputPath = r"D:\\workspace\\pycharm\\show-me-the-code\\show-me-the-code\\chapter01\\images\\";
     fontPath = r"D:\\workspace\\pycharm\\show-me-the-code\\show-me-the-code\\chapter01\\images\\Font\\";
@@ -19,10 +14,10 @@ def addcount():
     print(fontSize);
     # 增加文字
     fontobj = ImageFont.truetype(font = fontPath + "arial.ttf", size = fontSize, index = 0, encoding = '')
-    # fontobj = ImageFont.truetype(font=fontPath + "arial.ttc", size=fontSize, index=0, encoding='',
-    #                              filename=None)  # 实例字体对象
-    draw.text((image.size[0] - fontSize, 0), text="99", fill=(255, 0, 0), font=fontobj,
+    draw.text((image.size[0] - fontSize, 0), text="11", fill=(255, 0, 0), font=fontobj,
               anchor=None)  # 用draw对象的text()方法添加文字
     image.save(outputPath + outFile)  # 保存图片
+    return 0;
 
-addcount();
+if __name__ == '__main__':
+    addcount();
---
+++ questions.md
@@ -0,0 +1,3 @@
+chapter01:
+Q:在main函数中调用本地的方法竟然报错?python3的问题?
+A:fuck , main函数要放到最后
